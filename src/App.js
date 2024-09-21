a8938f0bed24e74df817c977f4d95c2cc0582a8e
Kaykay <kushagrakaushik1620@gmail.com>
2024-09-22 00:28:47 +0530

    delete extra pages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
-import RegisterTeacher from "./pages/register/RegisterTeacher.jsx";
-import RegisterStudent from "./pages/register/RegisterStudent.jsx";
-import LoginTeacher from "./pages/login/LoginTeacher.jsx";
-import LoginStudent from "./pages/login/LoginStudent.jsx";
-
 function App() {
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/register/t" element={<RegisterTeacher />} />
-          <Route path="/register/s" element={<RegisterStudent />} />
-          <Route path="/login/t" element={<LoginTeacher />} />
-          <Route path="/login/s" element={<LoginStudent />} />
         </Routes>
       </Router>
     </>
